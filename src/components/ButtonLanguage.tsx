f7a30f34415cf80853ad1d0a7f625b0f12fdf943
jess <jessiewq1@gmail.com>
2024-05-02 23:43:59 -0300

    fix: translate section

--- src/components/ButtonLanguage.tsx
+++ src/components/ButtonLanguage.tsx
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
 import { useLanguageContext } from '@/context/languageContext';
+import GTranslateIcon from '@mui/icons-material/GTranslate';
+import EastIcon from '@mui/icons-material/East';
 
 export default function ButtonLanguage() {
 
@@ -20,6 +22,7 @@ export default function ButtonLanguage() {
       }}
     >
       <ButtonGroup variant="text" aria-label="text button group">
+        <h1> <GTranslateIcon color='primary' /> <EastIcon color='primary' /></h1>
         <Button onClick={() => setLanguage('en-US')} disabled={language == 'en-US'}>en</Button>
         <Button onClick={() => setLanguage('pt-BR')} disabled={language == 'pt-BR'}>pt</Button>
         <Button onClick={() => setLanguage('it-IT')} disabled={language == 'it-IT'}>it</Button>
